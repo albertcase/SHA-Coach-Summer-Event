aa8593b039e3b432ae807b0d959d2f75dc7c3b53
vincenwen <vincen.wen@samesamechina.com>
2018-05-31 19:18:35 +0800

    06

--- template/result.tpl.php
+++ template/result.tpl.php
@@ -49,7 +49,7 @@
                     <script src="http://cdn.minnie.coach.samesamechina.com/web/js/checkin.js"></script>
                     <script type="text/javascript">
                         var getDayEl = document.querySelector('.get-day'),
-                            rdate = '<?php print $item->date;?>';
+                            rdate = parseInt('<?php print $item->date;?>', 10);
                             alert(rdate);
                             alert(new Date(rdate));
                         getDayEl.innerHTML = getDay(rdate);
